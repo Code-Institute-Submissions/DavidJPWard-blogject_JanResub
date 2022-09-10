1b2f3d4325c18e3f808ded1b27bbe3be7b849a58
David Ward <davidward1307@gmail.com>
2022-09-10 10:25:29 +0000

    added ability to veiw posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slugParameter>/', views.PostDetail.as_view(), name='post_detail'),
 ]
