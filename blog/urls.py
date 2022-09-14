74b19a17dee8ac52aad625aca07f9695cb3434b3
David Ward <davidward1307@gmail.com>
2022-09-14 14:44:24 +0000

    added ability to liked posts

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slugParameter>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slugParameter>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
+from django.http import HttpResponseRedirect
 from .models import Post
 from .forms import CommentForm
 
@@ -18,7 +19,10 @@ class PostLike(View):
         if post.likes.filter(id=self.request.user.id).exists():
             post.likes.remove(request.user)
         else:
-            post.
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('post_detail', args=[slugParameter]))
+        
 
 
 class PostDetail(View):
