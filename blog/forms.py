2f9a0341bc9081e8b4c66a959ba583c1089e6b55
David Ward <davidward1307@gmail.com>
2022-09-22 20:49:30 +0000

    added docstrings

--- blog/forms.py
+++ blog/forms.py
@@ -1,32 +1,48 @@
-from .models import Comment, Post, Profile, User
 from django import forms
+from .models import Comment, Post, Profile, User
 
 
 class CommentForm(forms.ModelForm):
+    """
+    form for commenting on posts
+    """
     class Meta:
         model = Comment
         fields = ('body',)
 
 
 class CreatePostForm(forms.ModelForm):
+    """
+    form for creating a post
+    """
     class Meta:
         model = Post
-        fields = ('title', 'content', 'excerpt', 'featured_image', 'category', 'status')
+        fields = ('title', 'content', 'excerpt', 'featured_image', 'category',
+                  'status')
 
 
 class EditUserForm(forms.ModelForm):
+    """
+    form for editing user infomation
+    """
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name')
 
 
 class EditProfileForm(forms.ModelForm):
+    """
+    form for editing user profile infomation
+    """
     class Meta:
         model = Profile
         fields = ('user_bio',)
-    
-    
+
+
 class EditPostForm(forms.ModelForm):
+    """
+    form for editing a post
+    """
     class Meta:
         model = Post
         fields = ('title', 'content', 'excerpt', 'featured_image', 'category',)
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,38 @@
+"""
+veiw classes for project
+"""
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.core.paginator import Paginator
 from .models import Post, User
-from .forms import CommentForm, CreatePostForm, EditUserForm, EditProfileForm, EditPostForm
+from .forms import (
+    CommentForm,
+    CreatePostForm,
+    EditUserForm,
+    EditProfileForm,
+    EditPostForm,
+)
 
-# Create your views here.
 
 class PostList(generic.ListView):
+    """
+    view that handles the home page, getting the list of all posts,
+    ordered by most recent and shows them in a list veiw
+    """
     model = Post
-    queryset = Post.objects.filter(status=1).order_by('-post_date')
-    template_name = 'index.html'
+    queryset = Post.objects.filter(status=1).order_by("-post_date")
+    template_name = "index.html"
     paginate_by = 6
 
+
 class PostLike(View):
+    """
+    view that handles the liking function, it retreives the post from
+    the model and adds the user id to the list of users who have liked
+    the post
+    """
     def post(self, request, slugParameter):
         post = get_object_or_404(Post, slug=slugParameter)
 
@@ -24,19 +42,22 @@ class PostLike(View):
         else:
             messages.success(request, "liked")
             post.likes.add(request.user)
-        
-        return HttpResponseRedirect(reverse('post_detail', args=[slugParameter]))
 
-class PostFilter(View):
-    def post(self, request, category):
-        post = Post.objects.filter(category=category)
+        return HttpResponseRedirect(reverse("post_detail",
+                                            args=[slugParameter]))
 
 
 class SubToUser(View):
+    """
+    view that handles the subscribe function, toggles the user from
+    subscribed to unsubscribed from a given user passed in as an argument
+    it add the requesting user to the given users 'subscriber list' and adds
+    the given user to the requesting users 'subscribed to' list
+    """
     def post(self, request, author, slugParameter):
         user = get_object_or_404(User, username=author)
         print(user)
-        #messages.info(request, author)
+        # messages.info(request, author)
         if user.profile.subscribers.filter(id=self.request.user.id).exists():
             user.profile.subscribers.remove(request.user)
             request.user.profile.subscribed_to.remove(user)
@@ -45,26 +66,27 @@ class SubToUser(View):
             user.profile.subscribers.add(request.user)
             request.user.profile.subscribed_to.add(user)
             messages.success(request, f"Subscribed to {user}")
-        
-        return HttpResponseRedirect(reverse('post_detail', args=[slugParameter]))
 
-
+        return HttpResponseRedirect(reverse("post_detail", args=[slugParameter]))
 
 
 class PostDetail(View):
-
+    """
+    view that shows the details of a post that a requesting user has clicked on
+    """
     def get(self, request, slugParameter, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slugParameter)
-        comments = post.comment.filter(approved=True).order_by('post_date')
-        #---likes---
+        comments = post.comment.filter(approved=True).order_by("post_date")
+        # ---likes---
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-            
-        #---subs---
+
+        # ---subs---
         subbed = False
         if post.author.profile.subscribers.filter(id=self.request.user.id).exists():
+
             subbed = True
 
         return render(
@@ -76,18 +98,18 @@ class PostDetail(View):
                 "commented": False,
                 "liked": liked,
                 "subbed": subbed,
-                "comment_form": CommentForm()
-            }
+                "comment_form": CommentForm(),
+            },
         )
 
     def post(self, request, slugParameter, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slugParameter)
-        comments = post.comment.filter(approved=True).order_by('post_date')
+        comments = post.comment.filter(approved=True).order_by("post_date")
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
@@ -106,17 +128,21 @@ class PostDetail(View):
                 "comments": comments,
                 "commented": True,
                 "liked": liked,
-                "comment_form": CommentForm()
-            }
+                "comment_form": CommentForm(),
+            },
         )
 
 
 class Profile(View):
+    """
+    view that shows the profile of a user that the requesting user
+    has clicked on
+    """
     def get(self, request, user):
         user = get_object_or_404(User, username=user)
         user_posts = Post.objects.filter(author=user)
-        
-        subscription_posts = Post.objects.none() 
+
+        subscription_posts = Post.objects.none()
         if user.profile.number_subbed_to() != 0:
             if user.profile.subscribed_to:
                 for sub in user.profile.subscribed_to.all():
@@ -126,16 +152,15 @@ class Profile(View):
         user_post_paginator = Paginator(user_posts, 3)
         sub_post_paginator = Paginator(subscription_posts, 3)
 
-        page_number = request.GET.get('page')
+        page_number = request.GET.get("page")
         user_post_page_obj = user_post_paginator.get_page(page_number)
         sub_post_page_obj = sub_post_paginator.get_page(page_number)
-
 
         user_post_pagination = True if user_posts.count() > 3 else False
         sub_post_pagination = True if subscription_posts.count() > 3 else False
 
         number_of_posts = user_posts.count()
-        
+
         print(subscription_posts.count())
 
         context = {
@@ -150,49 +175,67 @@ class Profile(View):
 
 
 class CreatePost(View):
+    """
+    view that shows a form that, once submitted, creates a post, only available
+    when the user is logged in
+    """
     def get(self, request):
-        return render(request, 'create_post.html', {'form': CreatePostForm})
-    
-    def post(self,request):
+        return render(request, "create_post.html", {"form": CreatePostForm})
+
+    def post(self, request):
         create_post_form = CreatePostForm(request.POST, request.FILES)
         if create_post_form.is_valid():
             create_post_form.instance.author = request.user
-            post_slug = ''.join(e for e in create_post_form.instance.title if e.isalnum())
+            post_slug = "".join(
+                e for e in create_post_form.instance.title if e.isalnum()
+            )
             create_post_form.instance.slug = post_slug
-            #create_post_form.instance.featured_image = request.FILES["file"]
+            # create_post_form.instance.featured_image = request.FILES["file"]
             create_post_form.save()
-        return redirect('home')
+        return redirect("home")
+
 
 class EditProfile(View):
+    """
+    view that allows a user to edit their profile, shows both the user form
+    and the user profile form.
+    """
     def get(self, request):
 
         edit_user_form = EditUserForm(instance=request.user)
         edit_profile_form = EditProfileForm(instance=request.user.profile)
 
-        return render(request, 'edit_profile.html', {'user_form': edit_user_form, 'profile_form': edit_profile_form})
-    
+        return render(
+            request,
+            "edit_profile.html",
+            {"user_form": edit_user_form, "profile_form": edit_profile_form},
+        )
+
     def post(self, request):
-        edit_user_form = EditUserForm(request.POST, instance=user)
-        edit_profile_form = EditProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        edit_user_form = EditUserForm(request.POST, instance=request.user)
+        edit_profile_form = EditProfileForm(
+            request.POST, request.FILES, instance=request.user.profile
+        )
         if edit_user_form.is_valid() and edit_profile_form.is_valid():
             edit_user_form.save()
             edit_profile_form.save()
-        return redirect('profile')
+        return redirect("profile")
 
 
 class EditPost(View):
-    
+    """
+    view that allows a user to edit a post they have made.
+    """
     def get(self, request, title):
-        print("poop" + title)
         post_to_edit = get_object_or_404(Post, title=title)
 
         edit_post_form = EditPostForm(instance=post_to_edit)
 
-        return render(request, 'edit_post.html', {'edit_post_form': edit_post_form})
+        return render(request, "edit_post.html", {"edit_post_form": edit_post_form})
 
     def post(self, request):
         edit_post_form = EditPostForm(request.POST, request.FILES)
         if edit_post_form.is_valid():
             edit_post_form.save()
 
-        return redirect('profile')
+        return redirect("profile")
